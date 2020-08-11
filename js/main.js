681abffe1cf419d6ff170d79dccb10e64d1b1b1e
Reactivee <idadajon@gmail.com>
2020-08-11 05:29:05 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// window.onscroll = function() {scrollFunction()};
+// 	// console.log("asds")
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+// 	console.log("asds")
+//     // document.getElementById("navbar-menu").style.background-color = #EDEDED;
+//     document.getElementById("navbar-menu").style.color = "black";    
+//   } else {
+//     // document.getElementById("navbar-menu").style.background-color = "transparent";
+//     document.getElementById("navbar-menu").style.color = "#D0D5D7";
+//   }
+// }
+
+	 

9213472612f674dc5b0d1337439b350902fd224a
iReact <idadajon@gmail.com>
2021-06-22 14:12:12 +0500

    push

---
+++ js/push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `0c457b869c91fed93a5d672c4beb5782`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
